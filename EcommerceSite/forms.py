a34abb55005a96d4a55fd9475064d97823fc5b1e
Hessah AY <ay.hessah@gmail.com>
2025-11-26 18:35:46 +0300

    update

--- EcommerceSite/forms.py
+++ EcommerceSite/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(label="Username")
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
-    email = forms.EmailField(label="Email") 
-    
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control mb-3',
+            'placeholder': 'Username'
+        }),
+        label=''
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control mb-3',
+            'placeholder': 'Password'
+        }),
+        label=''
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control mb-3',
+            'placeholder': 'Email'
+        }),
+        label=''
+    )
